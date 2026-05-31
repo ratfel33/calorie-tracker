68a32185033f6ce1390d1d3fcfa6dfb7cf7446d3
ratfel33 <ratfel_sc@hotmail.com>
2026-05-31 01:21:45 -0600

    update to SP

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -12,4 +12,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 // 3. Initialize the client securely
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl!, supabaseAnonKey!);
